a90b82a129f831881c73c3c4ee2450f1953af5a6
dwight fellman <fellmad@gmail.com>
2014-11-02 12:22:12 -0600

    more stuff

---
+++ src/05_type_info/main.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <tchar.h>
+#include <string>
+#include <iostream>
+
+class A {
+	std::string a;
+	long long b;
+};
+
+class B : public A {};
+
+int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+{
+	auto a = A();
+	std::cout << typeid(A).name() << std::endl;
+	std::cout << typeid(a).name() << std::endl;
+
+	const auto b = B();
+	std::cout << typeid(B).name() << std::endl;
+	std::cout << typeid(b).name() << std::endl;
+
+	std::cout << typeid(std::wstring).name() << std::endl;
+	std::cout << typeid(std::cout).name() << std::endl;
+
+	std::cout << std::endl << "Press any key !";
+	getchar();
+
+	return 0;
+}
