567a039e3f930b336fa8c7265024559dc21b9543
dwight <fellmad@gmail.com>
2014-11-06 10:31:31 -0600

    split working using c++11 raw string literal

--- src/11_regex/regex.cpp
+++ src/11_regex/regex.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include <regex>
 
-void show_matches(const std::string& in, const std::string& re)
-{
-    std::smatch m;
-    std::regex_search(in, m, std::regex(re));
-    if (m.empty()) {
-        std::cout << "input=[" << in << "], regex=[" << re << "]: NO MATCH\n";
-    }
-    else {
-        std::cout << "input=[" << in << "], regex=[" << re << "]: ";
-        std::cout << "prefix=[" << m.prefix() << "] ";
-        for (std::size_t n = 0; n < m.size(); ++n)
-            std::cout << " m[" << n << "]=[" << m[n] << "] ";
-        std::cout << "suffix=[" << m.suffix() << "]\n";
-    }
-}
 int main()
 {
-    show_matches("abcdef", "abc|def");
-    show_matches("abc", "ab|abc"); // left Alernative matched first
+    // std::regex pathSplitRegex("\\s*[;]+\\s*");       // old: ugly escapes: \\
 
-    // Match of the input against the left Alternative (a) followed
-    // by the remained of the regex (c|bc) succeeds, which results
-    // in m[1]="a" and m[4]="bc".
-    // The skipped Alternatives (ab) and (c) leave their submatches
-    // m[3] and m[5] empty.
-    show_matches("abc", "((a)|(ab))((c)|(bc))");
+    std::regex pathSplitRegex(R"(\s*[;]+\s*)"); // c++11 raw literal
+
+    std::string path = R"(c:\temp; d:\temp; c:\tmep\123 abc; )";
+
+    std::vector <std::string> folderNames;
+
+    const std::sregex_token_iterator end;
+
+    for (
+        std::sregex_token_iterator iter(path.begin(), path.end(), pathSplitRegex, -1);
+        iter != end;
+        ++iter)
+    {
+        folderNames.push_back(*iter);
+    }
+
+    for (auto folderName : folderNames) {
+        std::cout << folderName << std::endl;
+    }
 }
