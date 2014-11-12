df95100e88a636bf34b54642e05dc34fae7ede96
dwight <fellmad@gmail.com>
2014-11-12 09:04:12 -0600

    tuple

--- src/make_tuple/make_tuple.cpp
+++ src/make_tuple/make_tuple.cpp
@@ -2,7 +2,11 @@
 #include <string>
 #include <tuple>
 
-typedef std::tuple <bool, std::string, float> CommonTuple;
+// old:
+// typedef std::tuple <bool, std::string, float> CommonTuple;
+
+// C++11: we're having fun:
+using CommonTuple = std::tuple <bool, std::string, float>;
 
 CommonTuple f1() {
     CommonTuple tup {true, "string from f1", 1.234f};
@@ -13,12 +17,22 @@ CommonTuple f2() {
   return std::forward_as_tuple(false, "f2 string", 0.0f);
 }
 
+CommonTuple f3() {
+    bool aBool {false};
+    std::string aString {"a string"};
+    float floater {1.234f};
+    auto tup = std::make_tuple(aBool, aString, floater);
+    return tup;
+}
+
 int main()
 {
-
     auto result1 = f1();
     std::cout << typeid(result1).name() << std::endl;
 
     auto result2 = f2();
     std::cout << typeid(result2).name() << std::endl;
+
+    auto result3 = f3();
+    std::cout << typeid(result3).name() << std::endl;
 }
