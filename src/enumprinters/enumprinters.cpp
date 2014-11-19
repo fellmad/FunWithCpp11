895c728b6933290bcff33cbb5a93a198b8ea3f9a
dwight <fellmad@gmail.com>
2014-11-19 09:17:23 -0600

    renames

---
+++ src/enumprinters/enumprinters.cpp
@@ -0,0 +1,72 @@
+#include <windows.h>
+#include <tchar.h>
+#include <winspool.h>
+#include <string>
+#include <assert.h> // not C++ standard
+#include <vector>
+#include <iostream>
+
+#pragma comment(lib, "winspool.lib")  // MS pragmas: http://msdn.microsoft.com/en-us/library/d9x1s805.aspx
+
+////////////////////////////////////////////////////////////////////////////////
+// use win32 EnumPrinters() to return a list of printer names.
+// level 2 / PRINTER_INFO_2 hardcoded.
+////////////////////////////////////////////////////////////////////////////////
+std::vector <std::wstring> GetPrinterNames()
+{
+
+    // 1) get size needed for printernames buffer:
+    const DWORD flags = PRINTER_ENUM_LOCAL | PRINTER_ENUM_NETWORK | PRINTER_ENUM_CONNECTIONS;
+    DWORD neededByteCount(0);
+    DWORD returnedByteCount(0);
+    const DWORD enumLevel(2);
+
+    BOOL rc = ::EnumPrinters(
+        flags,
+        nullptr,	// use local server
+        enumLevel,
+        nullptr,	// no output buffer
+        0,			// ask for zero bytes
+        &neededByteCount,
+        &returnedByteCount);
+
+    assert(FALSE == rc);
+
+    DWORD lastError = ::GetLastError();
+    assert(ERROR_INSUFFICIENT_BUFFER == lastError);
+
+    // 2) get the list of printernames.
+    const DWORD printerInfo2BufferSize(neededByteCount);
+    std::vector <byte> printerInfo2Bytes(printerInfo2BufferSize, 0);
+
+    rc = ::EnumPrintersW(	// force UNICODE flavor regardless of build
+        flags,
+        nullptr,			// use local server
+        enumLevel,
+        &printerInfo2Bytes[0],
+        printerInfo2BufferSize,
+        &neededByteCount,
+        &returnedByteCount);
+
+    assert(rc);
+    auto printerInfo2Ptr = reinterpret_cast <PPRINTER_INFO_2> (&printerInfo2Bytes[0]);
+
+    std::vector <std::wstring> printerNames;
+
+    for (DWORD printerIndex = 0; printerIndex < returnedByteCount; printerIndex++) {
+        std::wstring printerName = printerInfo2Ptr[printerIndex].pPrinterName;
+        printerNames.push_back(printerName);
+    }
+
+    return printerNames;
+}
+
+int main()
+{
+    const auto printerNames = GetPrinterNames();
+    std::wcout << printerNames.size() << " printers installed:" << std::endl;
+    for (auto printerName : printerNames) {
+        std::wcout << " " << printerName << std::endl;
+    }
+    return 0;
+}
---
+++ src/factorial_thread/factorial_thread.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+unsigned long long factorial(const unsigned long long currentNumber)  // C++11: 64-bit
+{
+    if (currentNumber <= 1)
+    {
+        return 1;
+    }
+
+    const auto result = currentNumber * factorial(currentNumber - 1);
+    return result;
+}
+
+void sayHello(const int number, const short factorial_count)
+{
+    std::cout << "hello from a c++ 11 thread dude." << std::endl;
+    std::cout << "getting factorial of " << number
+              << " " << factorial_count << " times." << std::endl;
+
+    auto factorial_result = factorial(number);
+    for (int factorialIndex(0); factorialIndex < 10000000; factorialIndex++) {
+        factorial_result = factorial(number);
+    }
+    std::cout << "factorial of " << number << " is " << factorial_result << std::endl;
+}
+
+int main()
+{
+    std::cout.sync_with_stdio(true); // make std::cout thread-safe
+
+    const auto concurrency = std::thread::hardware_concurrency();
+    std::cout << "on this box, the number of hardware thread contexts is: " << concurrency << std::endl;
+    std::cout << "main() thread id: " << std::this_thread::get_id() << std::endl;
+
+    std::thread hello_thread(sayHello, 40, 50);
+    const auto thisThreadID = hello_thread.get_id();
+    std::cout << "thread id " << thisThreadID << " launched." << std::endl;
+
+    hello_thread.join();
+    std::cout << "back on the main thread - " << thisThreadID << std::endl;
+
+    return 0;
+}
