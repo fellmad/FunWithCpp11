1198c7f5e7b9dc1093f816faab7c2c5f2133cb46
dwight fellman <fellmad@gmail.com>
2014-11-30 10:33:35 -0600

    cleanup

--- src/utility_apps/chars/chars.cpp
+++ src/utility_apps/chars/chars.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <iomanip>
 
+void output_a_line(const unsigned short& val)
+{
+    std::cout << std::setw(4);
+    std::cout << std::dec << val << " ";
+    std::cout << std::setw(4);
+    std::cout << std::hex << val << " ";
+    std::cout << std::setw(4);
+    std::cout << (char) val << std::endl;
+}
+
 int main()
 {
-    for (short b = 0; b < 256; b++) {
-        std::cout << std::setw(4);
-        std::cout << std::dec << b << " ";
-        std::cout << std::setw(4);
-        std::cout << std::hex << b << " ";
-        std::cout << std::setw(4);
-        std::cout << (char) b << std::endl;
+    for (unsigned short val = 0; val <= 255; val++) {
+        output_a_line(val);
     }
     return 0;
 }
