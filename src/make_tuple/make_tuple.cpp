8f1f51d13539ee17bdfb53ec4b2cc0261be13278
dwight fellman <fellmad@gmail.com>
2014-11-30 10:14:58 -0600

    tuples

--- src/make_tuple/make_tuple.cpp
+++ src/make_tuple/make_tuple.cpp
@@ -6,22 +6,26 @@
 // typedef std::tuple <bool, std::string, float> CommonTuple;
 
 // C++11: we're having fun:
-using CommonTuple = std::tuple <bool, std::string, float>;
+using CommonTuple = std::tuple < bool, std::string, float > ;
 
-CommonTuple f1() {
+CommonTuple f1()
+{
     CommonTuple tup {true, "string from f1", 1.234f};
     return tup;
 }
 
-CommonTuple f2() {
-  return std::forward_as_tuple(false, "f2 string", 0.0f);
+CommonTuple f2()
+{
+    // http://en.cppreference.com/w/cpp/utility/tuple/forward_as_tuple
+    return std::forward_as_tuple(false, "f2 string", 0.0f);
 }
 
-CommonTuple f3() {
+CommonTuple f3()
+{
     bool aBool {false};
     std::string aString {"a string"};
     float floater {1.234f};
-    auto tup = std::make_tuple(aBool, aString, floater);
+    const auto tup = std::make_tuple(aBool, aString, floater);
     return tup;
 }
 
@@ -30,7 +34,7 @@ int main()
     auto result1 = f1();
     std::cout << typeid(result1).name() << std::endl;
 
-    auto result2 = f2();
+    const auto result2 = f2();
     std::cout << typeid(result2).name() << std::endl;
 
     auto result3 = f3();
