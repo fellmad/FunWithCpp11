2d5096f4aef49fab4a92c728e3d94ca05cfe739d
dwight <fellmad@gmail.com>
2014-12-05 14:13:46 -0600

    lambda

--- src/lambda/lambda/lambda.cpp
+++ src/lambda/lambda/lambda.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+    [] {std::cout << "hello Lambda." << std::endl; }();
+
+    auto stringresult1 = [] (const std::string& str) -> std::string {
+        return "Hello from " + str;
+    } ("a parm to a lambda");
+    std::cout << "--- stringresult1: " << stringresult1 << std::endl;
+
+    auto stringresult2 = [] (const std::string& str) {
+        return "Hello from " + str;
+    } ("another lambda parm");
+    std::cout << "--- stringresult2: " << stringresult2 << std::endl;
+
+    // q: what is the difference between lines 8 and 13?
+
     auto doubleFunction = [=] (double x) { return  2.0 * x; };
     auto doubleResult = doubleFunction(4.1);
     std::cout << "--- doubleFunction(4.1): " << doubleResult << std::endl;
 
     // q: why is doubleFuntion's TYPE double?
     // a: hint: cast the return expression as an int...
-
 
     return 0;
 }
