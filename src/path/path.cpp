65176dca4f5bb97abcc12727db99cb7a79ae67b6
dwight <fellmad@gmail.com>
2014-12-10 08:36:33 -0600

    path

---
+++ src/path/path.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// 'borrowed' from Boost. This is bleeding-edge, but seems *very good*:
+// http://en.cppreference.com/w/cpp/experimental/fs
+////////////////////////////////////////////////////////////////////////////////
+
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::tr2::sys;
+
+int main()
+{
+    fs::path currentFolder {"."};
+
+    fs::directory_iterator directoryIterator(currentFolder);
+    fs::directory_iterator end;
+
+    std::cout << "current folder: " << std::endl;
+    for (; directoryIterator != end; ++directoryIterator) {
+        std::cout << directoryIterator->path() << std::endl;
+    }
+
+    fs::path threeDir {"c:/windows/system32"};
+    threeDir /= "spool";
+    threeDir /= "drivers/x64";
+    threeDir /= "3";
+
+    directoryIterator = threeDir;   // very nice
+
+    std::cout << "3dir: " << std::endl;
+    for (; directoryIterator != end; ++directoryIterator) {
+        std::cout << directoryIterator->path() << std::endl;
+    }
+
+    return 0;
+}
