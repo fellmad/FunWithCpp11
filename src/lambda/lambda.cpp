23e1579f34a1ae7eecaba25184867930d61a092e
dwight fellman <fellmad@gmail.com>
2014-12-14 12:23:50 -0600

    lambda; trailing return type

--- src/lambda/lambda.cpp
+++ src/lambda/lambda.cpp
@@ -10,19 +10,18 @@ int main()
     } ("a parm to a lambda");
     std::cout << "--- stringresult1: " << stringresult1 << std::endl;
 
+    // same: trailing return type inferred:
     auto stringresult2 = [] (const std::string& str) {
         return "Hello from " + str;
     } ("another lambda parm");
     std::cout << "--- stringresult2: " << stringresult2 << std::endl;
 
-    // q: what is the difference between lines 8 and 13?
-
-    auto doubleFunction = [=] (double x) { return  2.0 * x; };
+    auto doubleFunction = [=] (double x) { return 2.0 * x; };
     auto doubleResult = doubleFunction(4.1);
     std::cout << "--- doubleFunction(4.1): " << doubleResult << std::endl;
 
     // q: why is doubleFuntion's TYPE double?
-    // a: hint: cast the return expression as an int...
+    // a: hint: cast the return expression as an int...hover...
 
     return 0;
 }
---
+++ src/trailing_return_type/trailing_return_type/trailing_return_type.cpp
@@ -0,0 +1,53 @@
+// Prata, Stephen; Prata Stephen(2011 - 10 - 18).C++ Primer Plus(6th Edition)
+// (Developer's Library) (Kindle Locations 29980-29990). Pearson Education. Kindle Edition.
+
+#include <iostream>
+
+// weird. C++ standard library does not have constant for Pi.
+// do this at compile-time:
+// OUCH! not constexpr keyword NOT available in microsoft C++ 11. UGH.
+//constexpr double pi()
+//{
+//    // return std::atan(1) * 4;
+//    return 3.4;
+//}
+// see: http://en.cppreference.com/w/cpp/language/constexpr
+
+const double PI = std::atan(1.0) * 4;
+
+double f1(double d, int i)
+{
+    return d * i;
+}
+
+// for fun, rename this function to f1:
+auto f2(double d, int i) -> double
+{
+    return d * i;
+}
+
+// The problem that's addressed here is that T and U are not in scope before
+// the compiler reads the f3 parameter list, so any use of decltype has to come
+// after the parameter list. The new syntax makes that possible:
+template<typename T1, typename T2>
+auto f3(const T1& t1, const T2& t2) -> decltype(t1 * t2)
+{
+    auto retval = t1 * t2;
+    return retval;
+}
+
+int main()
+{
+    // note: heed the /W4 compiler warnings...
+
+    std::cout << "f1(PI, 2): " << f1(PI, 2) << std::endl;
+    std::cout << "f1(2, PI): " << f1(2, PI) << std::endl;
+
+    std::cout << "f2(PI, 2): " << f2(PI, 2) << std::endl;
+    std::cout << "f2(2, PI): " << f2(2, PI) << std::endl;
+
+    std::cout << "f3(PI, 2): " << f3(PI, 2) << std::endl;
+    std::cout << "f3(2, PI): " << f3(2, PI) << std::endl;
+
+    return 0;
+}
