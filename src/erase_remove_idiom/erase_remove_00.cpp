e0989a844bb839f0a42e330ef1bb25ed14a84510
dwight <fellmad@gmail.com>
2014-12-15 12:19:13 -0600

    cleanup

--- src/erase_remove_idiom/erase_remove_00.cpp
+++ src/erase_remove_idiom/erase_remove_00.cpp
@@ -19,7 +19,7 @@ void print(const std::vector<int> &vec)
 int main()
 {
     // initialises a vector that holds the numbers from 0-9.
-    std::vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::vector<int> v { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     print(v);
 
     // removes all elements with the value 5
