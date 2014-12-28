d1a5e4dc607ea354fe20c027c3aac4db16a24d9a
dwight fellman <fellmad@gmail.com>
2014-12-28 14:09:51 -0600

    lambdas...

---
+++ src/lambda_01/lambda_01.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iostream>
+
+// from: C++ standard: 5.1.2 Lambda expressions
+void abssort(float* x, unsigned N)
+{
+    std::sort(
+        x,
+        x + N,
+        [] (float a, float b) {return std::abs(a) < std::abs(b); }
+    );
+}
+
+int main()
+{
+    float naked_floats[] {
+            24.5f,
+             6.7f,
+             0.0f,
+         12345.56f,
+         -4321.45f
+    };
+
+    abssort(naked_floats, sizeof naked_floats / sizeof(float));
+
+    std::cout << "absolute-value sorted floats:" << std::endl;
+    for (const auto& floater : naked_floats) {
+        std::cout << floater << std::endl;
+    }
+}
