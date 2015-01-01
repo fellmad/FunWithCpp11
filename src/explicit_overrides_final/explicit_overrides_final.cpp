2cbe2ed715eb913e3ff3ef3a230a376d39125a04
dwight fellman <fellmad@gmail.com>
2015-01-01 11:05:51 -0600

    updates

---
+++ src/explicit_overrides_final/explicit_overrides_final.cpp
@@ -0,0 +1,32 @@
+struct Base {
+    virtual void some_func(float);
+};
+
+struct Derived : Base {
+    virtual void some_func(int);
+};
+
+// line 6: OUCH. compiler lets you override function with different signature?
+
+struct A {
+    virtual void some_func(float);
+};
+
+struct B  : A {
+    virtual void some_func(int) override; // ill-formed - doesn't override a base class method
+};
+
+// line 16: OK: get compile-time error
+
+class D {
+public:
+    virtual void foo() final;
+    void bar() final;
+};
+
+class E final : public D {
+    void foo();
+};
+
+class F : public E {
+};
