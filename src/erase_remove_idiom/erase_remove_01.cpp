d68d2fbc5887ebb2c6e420f512ed06704cbf5cf2
dwight fellman <fellmad@gmail.com>
2015-01-01 10:09:57 -0600

    cleanup

--- src/erase_remove_idiom/erase_remove_01.cpp
+++ src/erase_remove_idiom/erase_remove_01.cpp
@@ -5,20 +5,17 @@
 #include <algorithm>
 #include <numeric>
 
-auto is_odd(int i) -> bool
-{
+auto is_odd(int i) -> bool {
     return (i % 2) != 0;
 }
 
-void print(const std::vector <int> &vec)
-{
+void print(const std::vector <int> &vec) {
     for (const auto &i : vec)
         std::cout << i << ' ';
     std::cout << std::endl;
 }
 
-int main()
-{
+int main() {
     // initialises a vector that holds the numbers from 0-9.
     std::vector<int> v(10);
     std::iota(v.begin(), v.end(), 0);
@@ -32,3 +29,5 @@ int main()
     v.erase( std::remove_if(std::begin(v), std::end(v), is_odd), std::end(v) );
     print(v);
 }
+
+// line 21: std::iota()
