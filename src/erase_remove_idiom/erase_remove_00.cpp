602bc5de3de01f944540e2debe3ff80e3f70be50
dwight fellman <fellmad@gmail.com>
2015-01-01 10:06:42 -0600

    cleanup

--- src/erase_remove_idiom/erase_remove_00.cpp
+++ src/erase_remove_idiom/erase_remove_00.cpp
@@ -4,29 +4,30 @@
 #include <iostream>
 #include <algorithm>
 
-bool is_odd(int i)
-{
+bool is_odd(const int& i) {
     return (i % 2) != 0;
 }
 
-void print(const std::vector<int> &vec)
-{
+void print(const std::vector <int> &vec) {
     for (const auto &i : vec)
         std::cout << i << ' ';
     std::cout << std::endl;
 }
 
-int main()
-{
+int main() {
     // initialises a vector that holds the numbers from 0-9.
     std::vector<int> v { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     print(v);
 
     // removes all elements with the value 5
     v.erase( std::remove( std::begin(v), std::end(v), 5 ), std::end(v) );
+
     print(v);
 
     // removes all odd numbers
     v.erase( std::remove_if(std::begin(v), std::end(v), is_odd), std::end(v) );
     print(v);
 }
+
+// line 28 : let's use a lambda expression instead:
+// v.erase( std::remove_if(std::begin(v), std::end(v), [] (int i) {return i % 2 != 0;}), std::end(v));
