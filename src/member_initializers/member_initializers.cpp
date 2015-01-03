ac605c03b40466aaff528702b85f3a39543c7da2
dwight fellman <fellmad@gmail.com>
2015-01-03 15:45:03 -0600

    more

---
+++ src/member_initializers/member_initializers.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+class Printer {
+
+	// "non-static data member with initializer (C++11)"
+    int _jobCount = 3;
+    std::string _name = "xps card printer";
+
+    // i *sure wish* we could do this with strings:
+    static const unsigned short _maxJobs = 100;
+
+public:
+    void Dump() {
+        std::cout << _name << " " << _jobCount << std::endl;
+    }
+};
+
+int main() {
+    Printer p;
+    p.Dump();
+}
