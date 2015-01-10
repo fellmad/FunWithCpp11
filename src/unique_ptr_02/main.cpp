cbb9c1650d9c64e4efe8723d891f84392e00ed53
dwight fellman <fellmad@gmail.com>
2015-01-10 13:26:08 -0600

    updates

--- src/unique_ptr_02/main.cpp
+++ src/unique_ptr_02/main.cpp
@@ -6,58 +6,54 @@
 class Printer {
     std::string _printerName;
     HANDLE _printerHandle;
+
 public:
-    void print(const std::string jobName)
-    {};
+    void print(const std::string jobName) {
+        std::cout << jobName << ";";
+    };
 
     static int _destructor_called_count;
 
-    ~Printer()
-    {
+    ~Printer() {
         _destructor_called_count++;
     }
 };
 
 int Printer::_destructor_called_count {};
 
-void leak_a_printer()
-{
+void leak_a_printer() {
     Printer* pPrinter = new Printer {};
-    pPrinter->print("leaky printer job");
+    pPrinter->print("leeeekr");
 }
 
-void leak_a_printer_array()
-{
+void leak_a_printer_array() {
     Printer* printers = new Printer[10];
     delete printers; // see the bug?
 }
 
-void leak_a_printer_NOT()
-{
+void leak_a_printer_NOT() {
     std::unique_ptr <Printer> printer {new Printer {}};
-    printer->print("clean, fresh, unleaky print job");
+    printer->print("non-leek");
 }
 
-void leak_a_printer_array_NOT()
-{
+void leak_a_printer_array_NOT() {
     // You can use make_unique to create a unique_ptr to an array,
     // but you cannot use make_unique to initialize the array elements:
     auto printers = std::make_unique < Printer[] >(5);
     for (auto printer_index = 0; printer_index < 5; printer_index++) {
-        printers[printer_index].print("printing");
+        printers[printer_index].print("unleaker");
     }
 }
 
-int main()
-{
+int main() {
     _CrtMemState startMemState {};
     _CrtMemCheckpoint(&startMemState);
 
     for (auto index = 0; index < 10; index++) {
-        // leak_a_printer();
+        leak_a_printer();
         // leak_a_printer_array(); // will crash dbgcrt
         // leak_a_printer_NOT();
-        leak_a_printer_array_NOT();
+        // leak_a_printer_array_NOT();
     }
 
     _CrtMemState currentMemState {};
@@ -78,5 +74,4 @@ int main()
 #ifndef _DEBUG
     std::cerr << "try a DEBUG BUILD in vstudio." << std::endl;
 #endif
-
-    }
+}
