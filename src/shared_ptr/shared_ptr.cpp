a1379a6bf70d70d3a903eb6121571016d7c7d4d8
dwight fellman <fellmad@gmail.com>
2015-01-10 14:30:03 -0600

    cleanup

--- src/shared_ptr/shared_ptr.cpp
+++ src/shared_ptr/shared_ptr.cpp
@@ -1,41 +1,46 @@
-// stolen from: http://www.reddit.com/r/cpp/comments/2l202i/iterate_over_a_list_of_types/
+// munged from: http://www.reddit.com/r/cpp/comments/2l202i/iterate_over_a_list_of_types/
 
 #include <iostream>
 #include <string>
 #include <memory>
 #include <vector>
 
-class IF
-{
+// abstract base class ("interface" in any other language...):
+class IF {
 public:
     virtual std::string str() = 0;
 };
 
-class A : public IF
-{
+class A : public IF {
 public:
     std::string str() {
         return "A";
     }
 };
 
-class B : public IF
-{
+class B : public IF {
 public:
     std::string str() {
         return "B";
     }
 };
 
-int main()
-{
-    std::vector<std::shared_ptr<IF>> list {
+int main() {
+
+    // create a list of interface pointers:
+
+    std::vector<std::shared_ptr<IF>> pointer_list {
         std::make_shared<A>(),
         std::make_shared<B>(),
         std::make_shared<A>(),
         std::make_shared<A>()
     };
 
-    for (auto l : list)
-        std::cout << l->str() << std::endl;
+    for (const auto& pointer : pointer_list) {
+        std::cout << pointer->str() << std::endl;
+
+        const auto& another_instance {pointer};
+    }
 }
+
+// line 42: can't do that with unique_ptr
