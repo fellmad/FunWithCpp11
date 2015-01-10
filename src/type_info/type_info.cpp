8ccfe37041b3cdd95283a9cbaa8ca31e1e8043e0
dwight fellman <fellmad@gmail.com>
2015-01-10 13:36:19 -0600

    cleanup

--- src/type_info/type_info.cpp
+++ src/type_info/type_info.cpp
@@ -5,33 +5,35 @@
 
 auto multiply_two_ints(const int& a, const int& b) -> int
 {
-	const int result = a * b;
-	return result;
+    const int result = a * b;
+    return result;
 }
 
 class A {
-	std::string a;
-	long long b;
+    std::string a;
+    long long b;
 };
 
 class B : public A {};
 
-int main()
-{
-	auto a = A();
+int main() {
 
-	std::cout << typeid(A).name() << std::endl;
-	std::cout << typeid(a).name() << std::endl;
+    auto a = A();
 
-	const auto b = B();
-	std::cout << typeid(B).name() << std::endl;
-	std::cout << typeid(b).name() << std::endl;
+    std::cout << typeid(A).name() << std::endl;
+    std::cout << typeid(a).name() << std::endl;
 
-	std::cout << typeid(std::wstring).name() << std::endl;
-	std::cout << typeid(std::cout).name() << std::endl;
+    const auto b = B();
+    std::cout << typeid(B).name() << std::endl;
+    std::cout << typeid(b).name() << std::endl;
 
-	std::cout << typeid(multiply_two_ints).name() << std::endl;
- 
- 	std::cout << std::endl << "Press any key. Do it.";
-	getchar();
+    std::cout << typeid(std::wstring).name() << std::endl;
+    std::cout << typeid(std::cout).name() << std::endl;
+
+    std::cout << typeid(multiply_two_ints).name() << std::endl;
+
+    std::cout << std::endl << "press. a. key.";
+    ::getchar();
 }
+
+// line 6: C++11 trailing return type
