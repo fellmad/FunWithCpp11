d5709c58453895ae40efeb00b5f3a0cad0283118
dwight fellman <fellmad@gmail.com>
2015-01-10 14:54:36 -0600

    cleanup

--- src/range_based_for/range_based_for.cpp
+++ src/range_based_for/range_based_for.cpp
@@ -3,10 +3,10 @@
 #include <vector>
 
 template <typename T>
-std::ostream& operator<< (std::ostream& os, const std::vector<T>& vec)
+std::ostream& operator << (std::ostream& os, const std::vector<T>& vec)
 {
-    for (auto& el : vec) {
-        os << el << ' ';
+    for (const auto& el : vec) {
+        os << el << std::endl;
     }
     return os;
 }
@@ -23,6 +23,11 @@ int main()
         std::to_string(_MSC_FULL_VER),
         "dude."
     };
-
     std::cout << words << std::endl;
+
+    std::vector <int> numbers {-6, 3, 5, -22, 0 };
+    std::cout << numbers << std::endl;
 }
+
+// line 5: generic extraction operator
+// line 8: prevent copy constructor by using a reference
