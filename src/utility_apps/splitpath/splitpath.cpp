2c5df403985fde59ea6d50b1ea08fa32843b43b4
dwight fellman <fellmad@gmail.com>
2015-01-10 13:12:59 -0600

    cleanup

--- src/utility_apps/splitpath/splitpath.cpp
+++ src/utility_apps/splitpath/splitpath.cpp
@@ -1,39 +1,43 @@
-// old way of naming was <stdlib.h>; we get namespace of std too:
-#include <cstdlib>
+// munged from: http://stackoverflow.com/questions/236129/split-a-string-in-c
+
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <memory>
 
-// stolen from: http://stackoverflow.com/questions/236129/split-a-string-in-c
-template<typename T> std::vector<T> split(const T & str, const T & delimiters)
+template <typename T>
+std::vector <T> split(const T & str, const T & delimiters)
 {
-    std::vector<T> v;
+    std::vector<T> individual_items;
     T::size_type start = 0;
     auto pos = str.find_first_of(delimiters, start);
     while (pos != T::npos) {
         if (pos != start) // ignore empty tokens
-            v.emplace_back(str, start, pos - start);
+            individual_items.emplace_back(str, start, pos - start);
         start = pos + 1;
         pos = str.find_first_of(delimiters, start);
     }
 
     if (start < str.length()) {
         // ignore trailing delimiter
-        v.emplace_back(str, start, str.length() - start); // add what's left of the string
+        individual_items.emplace_back(str, start, str.length() - start); // add what's left of the string
     }
-
-    return v;
+    return individual_items;
 }
 
-int main()
-{
-    const char* envPtr = std::getenv("path");
-    if (nullptr == envPtr) {
-        std::cerr << "failed to get path" << std::endl;
+int main() {
+    size_t requred_size {};
+    getenv_s(&requred_size, nullptr, 0, "path");
+    if (0 == requred_size) {
+        std::cerr << "env var path not found" << std::endl;
         return -1;
     }
 
-    std::string envPath(envPtr);
+    std::unique_ptr <char> buf {new char[requred_size] {} };
+
+    ::getenv_s(&requred_size, buf.get(), requred_size, "path");
+
+    std::string envPath(buf.get());
     auto paths = split <std::string>(envPath, ";");
     for (auto path : paths) {
         std::cout << path << std::endl;
