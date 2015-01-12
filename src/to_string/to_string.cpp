c0521bd137cfce241720004e87444fb8c9aac0f0
dwight <fellmad@gmail.com>
2015-01-12 15:42:07 -0600

    to_string

--- src/to_string/to_string.cpp
+++ src/to_string/to_string.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <atlstr.h>
 
-// from some production code:
+// from some production code (NOT fun):
 CComBSTR ULONG2String(ULONG value) {
     CComBSTR bstrStr;
     CString s;
@@ -14,7 +14,7 @@ CComBSTR ULONG2String(ULONG value) {
 
 // using std::to_string. FUN!
 CComBSTR ULongToCComBSTR(const ULONG& value) {
-    const auto stringVal(std::to_string(value));
+    const auto& stringVal(std::to_string(value));
     const CComBSTR ccomBstrVal {stringVal.c_str()};
     return ccomBstrVal;
 }
@@ -36,8 +36,14 @@ int main() {
 
     auto s3(ULongToCComBSTR2(789));
     std::wcout << s3.m_str << std::endl;
+
+    // more FUN: strings to numbers!
+    const auto& num1 = std::stoi(" -123   ");
+    const auto& num2 = std::stod("    456.78e-43 ");
+    std::cout << "num1, num2: " << num1 << ", " << num2 << std::endl;
 }
 
 // 1) can we make a generic function - returning CString, CStringA /  W, 
 //    CComBSTR, std::string, std::wstring?
+//
 // 2) ULongToCComBSTR2() returns a reference
