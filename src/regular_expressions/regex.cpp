90a1bb4cf9e611f631c4b319c8461659f132124e
dwight <fellmad@gmail.com>
2016-01-21 10:12:42 -0600

    regex unicode; .gitignore

--- src/regular_expressions/regex.cpp
+++ src/regular_expressions/regex.cpp
@@ -5,23 +5,22 @@ int main()
 {
     // std::regex pathSplitRegex("\\s*[;]+\\s*");   // old: ugly escapes: \\
 
-    std::regex pathSplitRegex(R"(\s*[;]+\s*)");     // c++11 raw literal (FUN!)
+    std::wregex pathSplitRegex(LR"(\s*[;]+\s*)");     // c++11 raw literal (FUN!)
 
-    std::string path = R"(c:\temp;d:\temp;c:\temp\123 abc;f:/a/b/c/d/j.txt;x:\y/z\w)";
+    std::wstring path = LR"(c:\temp;d:\temp;c:\temp\123 abc;f:/a/b/c/d/j.txt;x:\y/z\w)";
 
-    std::vector <std::string> folderNames;
+    std::vector <std::wstring> folderNames;
 
-    const std::sregex_token_iterator end;
+    const std::wsregex_token_iterator end;
 
     for (
-        std::sregex_token_iterator iter(path.begin(), path.end(), pathSplitRegex, -1);
+        std::wsregex_token_iterator iter(path.begin(), path.end(), pathSplitRegex, -1);
         iter != end;
-        ++iter)
-    {
+        ++iter) {
         folderNames.push_back(*iter);
     }
 
     for (auto folderName : folderNames) {
-        std::cout << folderName << std::endl;
+        std::wcout << folderName << std::endl;
     }
 }
