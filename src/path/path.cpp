3780e2425edf3af56daa6a431105be67f7eadc06
dwight <fellmad@gmail.com>
2016-11-26 09:17:28 -0600

    vs2017

--- src/path/path.cpp
+++ src/path/path.cpp
@@ -1,37 +1,34 @@
 ////////////////////////////////////////////////////////////////////////////////
 // http://en.cppreference.com/w/cpp/experimental/fs
+// https://msdn.microsoft.com/en-us/library/dn986850.aspx
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <filesystem>
 #include <iostream>
 
-namespace fs = std::tr2::sys;   // 2014: <filesystem> approved by ISO c++ committee
+// nov 11 2016: either of these namespace decl's work:
+// namespace fs = std::tr2::sys;   // 2014: <filesystem> approved by ISO c++ committee
+namespace fs = std::experimental::filesystem::v1;
 
 int main() {
 
-    const fs::path currentFolder {"."};
-
-    fs::directory_iterator directoryIterator(currentFolder);
-    const fs::directory_iterator end;
+    const fs::path currentFolder{ "." };
+    fs::path::iterator pathIterator(currentFolder.begin());
 
     std::cout << "current folder: " << std::endl;
-    for (; directoryIterator != end; ++directoryIterator) {
-        std::cout << directoryIterator->path() << std::endl;
+    for (; pathIterator != currentFolder.end(); ++pathIterator) {
+        std::cout << *pathIterator << std::endl;
     }
 
-    fs::path threeDir {"c:/windows/system32"};
+    fs::path threeDir{ "c:/windows/system32" };
     threeDir /= "spool";
     threeDir /= "drivers/x64";
     threeDir /= "3";
 
-    directoryIterator = threeDir;
+    pathIterator = threeDir.begin();
 
     std::cout << "3dir: " << std::endl;
-    for (; directoryIterator != end; ++directoryIterator) {
-        std::cout << directoryIterator->path() << std::endl;
+    for (; pathIterator != threeDir.end(); ++pathIterator) {
+        std::cout << *pathIterator << std::endl;
     }
 }
-
-// use fs::wpath, fs::wdirectory_iterator, ... for unicode
-// line 23: 'append operator' defined for path class.
-// line 27: nice: assignment operator for path object to iterator object
