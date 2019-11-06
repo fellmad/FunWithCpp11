a0e849bb7a73e4e959bb60c521bb098951cadd22
dwight fellman <fellmad@gmail.com>
2019-11-06 09:21:23 -0600

    vs2019 sln

--- src/unique_ptr_02/main.cpp
+++ src/unique_ptr_02/main.cpp
@@ -3,65 +3,76 @@
 #include <iostream>
 #include <memory>
 
-class Printer {
+class Printer
+{
     std::string _printerName;
     HANDLE _printerHandle;
 
 public:
-    void print(const std::string jobName) {
+    void print(const std::string jobName)
+    {
         std::cout << jobName << ";";
     };
 
     static int _destructor_called_count;
 
-    ~Printer() {
+    ~Printer()
+    {
         _destructor_called_count++;
     }
 };
 
-int Printer::_destructor_called_count {};
+int Printer::_destructor_called_count{};
 
-void leak_a_printer() {
-    Printer* pPrinter = new Printer {};
+void leak_a_printer()
+{
+    Printer* pPrinter = new Printer{};
     pPrinter->print("leeeekr");
 }
 
-void leak_a_printer_array() {
+void leak_a_printer_array()
+{
     Printer* printers = new Printer[10];
     delete printers; // see the bug?
 }
 
-void leak_a_printer_NOT() {
-    std::unique_ptr <Printer> printer {new Printer {}};
+void leak_a_printer_NOT()
+{
+    std::unique_ptr<Printer> printer{ new Printer{} };
     printer->print("non-leek");
 }
 
-void leak_a_printer_array_NOT() {
+void leak_a_printer_array_NOT()
+{
     // You can use make_unique to create a unique_ptr to an array,
     // but you cannot use make_unique to initialize the array elements:
-    auto printers = std::make_unique < Printer[] >(5);
-    for (auto printer_index = 0; printer_index < 5; printer_index++) {
+    auto printers = std::make_unique<Printer[]>(5);
+    for (auto printer_index = 0; printer_index < 5; printer_index++)
+    {
         printers[printer_index].print("unleaker");
     }
 }
 
-int main() {
-    _CrtMemState startMemState {};
+int main()
+{
+    _CrtMemState startMemState{};
     _CrtMemCheckpoint(&startMemState);
 
-    for (auto index = 0; index < 10; index++) {
+    for (auto index = 0; index < 10; index++)
+    {
         leak_a_printer();
         // leak_a_printer_array(); // will crash dbgcrt
         // leak_a_printer_NOT();
         // leak_a_printer_array_NOT();
     }
 
-    _CrtMemState currentMemState {};
+    _CrtMemState currentMemState{};
     _CrtMemCheckpoint(&currentMemState);
-    _CrtMemState result {};
+    _CrtMemState result{};
     auto memDiff = _CrtMemDifference(&result, &startMemState, &currentMemState);
 
-    if (memDiff > 0) {
+    if (memDiff > 0)
+    {
         std::cerr << "OUCH. we've got about " << result.lTotalCount << " bytes of leakage, dude." << std::endl;
         std::cerr << "destructor called " << Printer::_destructor_called_count << " times. " << std::endl;
         _CrtDumpMemoryLeaks();
